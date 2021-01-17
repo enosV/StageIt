dd9508a9e4191159d119255bc982a27cf7b1b3b3
Enos Vazquez <enosvazquez@gmail.com>
2021-01-16 20:58:10 -0800

    cleaned up comments

--- stageIt.py
+++ stageIt.py
@@ -8,14 +8,16 @@ Last = LastName.upper()
 FirstName = input("First name: ")
 First = FirstName.capitalize()
 
-#RegNo takes any given number and format its into Reg
+#RegNo currently take anything and splits into XX-XXXX ** Need to convert to integers only **
 RegNo = input("Registration Number: ")
 Reg = RegNo[:2] + '-' + RegNo[2:]
 
-#NewDir combines the input from the user
+#NewDir combines the 3 inputs from the user
 NewDir = (Last + ', ' + First + ' ' + str(Reg))
 
-#This is where we tell our program to change directory over to the the network path. The path needs to be mounted prior to setting the path. By default is commented and needs to be commented out to be used.
+#This is where we tell our program to change directory over to the the network path. 
+#The path needs to be mounted prior to setting the path. 
+#By default is commented and needs to be commented out to be used.
 #os.chdir(r'ENTER_NETWORK_PATH_HERE')
 
 #This will write it on the current working directory (this changes if a path is privided above)
